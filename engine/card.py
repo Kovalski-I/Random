2c1957ed7d4c9e0f74b5e98a37b3460a97ded1f8
Tim Koval <timofeykoval14@gmail.com>
2019-08-21 16:52:06 +0300

    Changing card.py and two .gitignore files

---
+++ engine/card.py
@@ -0,0 +1,11 @@
+from PyQt5 import QtGui
+import sys
+import name
+
+def choose_card():
+    app = QtGui.QGuiApplication(sys.argv)
+    images = {}
+    for track in range(1, 53):
+        card = QtGui.QPixmap('card{0}.png'.format(track))
+        images['{0}'.format(track)] = card
+    name.choose(images[i] for i in range(1, 53))
