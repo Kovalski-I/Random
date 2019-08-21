a46a3a6e34a53a927b5c97657a912e5ba43d6687
Tim Koval <timofeykoval14@gmail.com>
2019-08-21 17:30:09 +0300

    Setting an empty window

---
+++ run.py
@@ -0,0 +1,14 @@
+from PyQt5 import QtWidgets
+
+class MainWindow(QtWidgets.QWidget):
+    def __init__(self):
+        QtWidgets.QWidget.__init__(self)
+        self.setWindowTitle('Random')
+        self.resize(800, 450)
+
+if __name__ == '__main__':
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    mainWindow = MainWindow()
+    mainWindow.show()
+    sys.exit(app.exec_())
