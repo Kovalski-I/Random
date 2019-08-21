f8341434cd567bd6436b81c2878f14f7afe1fe5c
Tim Koval <timofeykoval14@gmail.com>
2019-08-21 19:00:01 +0300

    Greet Message and combo box on the main window

--- run.py
+++ run.py
@@ -1,10 +1,27 @@
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtGui, QtCore
 
 class MainWindow(QtWidgets.QWidget):
     def __init__(self):
         QtWidgets.QWidget.__init__(self)
         self.setWindowTitle('Random')
         self.resize(800, 450)
+        self.setStyleSheet('background-color: white')
+
+        self.font = QtGui.QFont('Segoi Ui', 20)
+
+        self.label = QtWidgets.QLabel('              Welcome!\nChoose a mode to get started')
+        self.label.setFont(self.font)
+
+        self.combo = QtWidgets.QComboBox()
+
+        self.combobox = QtWidgets.QHBoxLayout()
+        self.combobox.addSpacing(275)
+        self.combobox.addWidget(self.combo)
+        self.combobox.addSpacing(275)
+
+        self.box = QtWidgets.QVBoxLayout(self)
+        self.box.addWidget(self.label, alignment = QtCore.Qt.AlignCenter)
+        self.box.addLayout(self.combobox)
 
 if __name__ == '__main__':
     import sys
